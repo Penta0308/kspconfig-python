d92961943ef159e2fe189f355220338faff46029
Penta0308 <38977908+Penta0308@users.noreply.github.com>
2020-09-02 14:04:43 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="kspconfig", # Replace with your own username
-    version="1.1",
+    version="1.3",
     author="Penta0308",
     author_email="kevin03088@naver.com",
     description="Kerbal Space Program Craft and Cfg Loader",
