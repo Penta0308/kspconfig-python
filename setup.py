b76881f42585bc3eb434cf5bad896d59ec065273
Penta0308 <38977908+Penta0308@users.noreply.github.com>
2020-09-02 14:01:23 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="kspconfig-penta", # Replace with your own username
+    name="kspconfig", # Replace with your own username
     version="1.1",
     author="Penta0308",
     author_email="kevin03088@naver.com",
