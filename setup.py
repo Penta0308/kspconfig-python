c956741804602c6993c41bb075f687e93438a81b
Penta0308 <38977908+Penta0308@users.noreply.github.com>
2020-09-02 13:37:56 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="kspconfig-penta", # Replace with your own username
+    version="0.0.1",
+    author="Penta0308",
+    author_email="kevin03088@naver.com",
+    description="Kerbal Space Program Craft and Cfg Loader",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Penta0308/kspconfig-python",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+)
